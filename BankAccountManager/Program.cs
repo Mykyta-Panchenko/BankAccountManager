6120e09a4142cd329364841056c303eebcd7ee19
Mykyta Panchenko <ipz235_pma@student.ztu.edu.ua>
2025-02-07 10:43:47 +0200

    first-Update

---
+++ BankAccountManager/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class BankAccount
+{
+    public double balance;
+
+    public void Deposit(double amount)
+    {
+        balance += amount;
+        Console.WriteLine($"Deposited {amount}. New balance: {balance}");
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount > balance)
+        {
+            Console.WriteLine("Insufficient funds.");
+            return;
+        }
+
+        balance -= amount;
+        Console.WriteLine($"Withdrew {amount}. New balance: {balance}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BankAccount account = new BankAccount();
+        account.Deposit(100);
+        account.Withdraw(50);
+        account.Withdraw(70);
+    }
+}
